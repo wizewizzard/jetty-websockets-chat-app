2444e832d0771b81320cf4ea587ba686065f0220
wizewizzard <kingaal93@gmail.com>
2022-07-28 14:59:27 +0700

    Initial frontend
    ый

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+
+import Header from './components/static/Header';
+
+function App() {
+  
+  return (  
+    <Router>
+        <div className="app">
+
+        </div>
+    </Router>
+  );
+  
+}
+
+export default App;
