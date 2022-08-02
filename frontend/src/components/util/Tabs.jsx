e717172d50ba95d78e81d0f5f4408fad766f2261
wizewizzard <kingaal93@gmail.com>
2022-08-02 22:43:59 +0700

    Edit Tabs component
    Now uses tab object as index, instead of it ordinal number

--- frontend/src/components/util/Tabs.jsx
+++ frontend/src/components/util/Tabs.jsx
@@ -1,23 +1,27 @@
 import React, {useState} from 'react'
 
 import styles from './Tabs.module.css'
-export default function Tabs({tabs, initial = 0}) {
-    const [active, setActive] = useState(initial);
+export default function Tabs({tabs, activeTab = tabs[0]}) {
+    const [active, setActive] = useState(activeTab);
     console.log('Active tab is: ', active);
     return (
         <>
             <div className={styles['tab-names']}>
                 {tabs.map((t, i) => {
                     return (
-                        <div key = {i} className={[styles['tab-name'], i === active ? styles['active'] : null].join(' ')} onClick={() => {
-                            setActive(i);
-                        }}>
+                        <div key = {i} className={[styles['tab-name'], t === active ? styles['active'] : null].join(' ')} 
+                        onClick={() => { setActive(t);}}>
                             {t.name}
                         </div>
                         );
                 })}
             </div>
-            {tabs[active].content}
+            {
+                <div className={styles['tab-content']}>
+                    {active.content}
+                </div>
+                
+            }
         </>
     )
 }
