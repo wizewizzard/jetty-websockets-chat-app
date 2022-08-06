03020f150c3c435f998d9b509ab13fddc09e15e8
wizewizzard <kingaal93@gmail.com>
2022-08-06 21:12:14 +0700

    Fix Jackson mapper not supporting LocalDateTime

--- src/main/java/com/wu/chatserver/configuration/DtoMapping.java
+++ src/main/java/com/wu/chatserver/configuration/DtoMapping.java
@@ -1,17 +1,22 @@
 package com.wu.chatserver.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
-import javax.inject.Singleton;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class DtoMapping {
     @Produces
     @Dependent
+    @Default
     public ObjectMapper objectMapper(){
         ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
+        //mapper.findAndRegisterModules();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
 }
